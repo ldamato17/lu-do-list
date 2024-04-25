d8c3f000a39be38ea443a43dd432772e342cad1b
ldamato17 <l.damato17@studenti.unisa.it>
2024-04-25 16:36:04 +0200

    ottimizzazione del codice eliminando ridondanze e snellendo metodi

--- script.js
+++ script.js
@@ -1,88 +1,50 @@
-class LUDOList{
-    constructor(){
-        this.tasks=this.caricaTasks();
-        this.generaTasks();
-        console.log("tasks", this.tasks);
+class LUDOList {
+    constructor() {
+        this.tasks = JSON.parse(localStorage.getItem("tasks")) || [];
     }
 
-    aggiungiTask(task){
+    aggiungiTask(task) {
         this.tasks.push(task);
-        console.log("tasks", this.tasks);
-
-        const todo=document.getElementById("todo");
-        const list=document.getElementById("list");
-        const li=document.createElement("li");
-        li.appendChild(document.createTextNode(task));
-        list.appendChild(li);
-        todo.appendChild(list);
-        li.addEventListener("click", (e)=>{
-            this.terminaTask(task);
-            e.srcElement.remove();
-        }); 
         localStorage.setItem("tasks", JSON.stringify(this.tasks));
+        this.renderTasks();
     }
 
-    terminaTask(task){
-        console.log(`${task} terminato!`);
-        const index=this.tasks.indexOf(task);
+    resetAllTasks() {
+        this.tasks = [];
+        localStorage.setItem("tasks", JSON.stringify(this.tasks));
+        this.renderTasks();
+    }
+
+    completeTask(index) {
         this.tasks.splice(index, 1);
         localStorage.setItem("tasks", JSON.stringify(this.tasks));
+        this.renderTasks();
     }
 
-    caricaTasks(){
-        const tasks=localStorage.getItem("tasks");
-        if(tasks){
-            return JSON.parse(tasks);
-        }else{
-            return [];
-        }
-    }
-
-    generaTasks(){
-        this.tasks.forEach(task => {
-            const todo=document.getElementById("todo");
-            const list=document.getElementById("list");
-            const li=document.createElement("li");
-            li.appendChild(document.createTextNode(task));
-            list.appendChild(li);
-            todo.appendChild(list);
-            li.addEventListener("click", (e)=>{
-                this.terminaTask(task);
-                e.srcElement.remove();
-            });
+    renderTasks() {
+        const todo = document.getElementById("todo");
+        todo.innerHTML = ''; 
+        this.tasks.forEach((task, index) => {
+            const li = document.createElement('li');
+            li.textContent = task;
+            li.addEventListener('click', () => this.completeTask(index));
+            todo.appendChild(li);
         });
-    }
-
-    resetAllTasks(){
-        this.tasks=[];
-        localStorage.setItem("tasks", JSON.stringify(this.tasks));
-        const todo=document.getElementById("todo");
-        todo.innerHTML="";
     }
 }
 
-
 document.addEventListener('DOMContentLoaded', () => {
-    const addTask=document.getElementById("add");
-    const rstTask=document.getElementById("rst");
-    const input=document.getElementById("input");
+    const ludoList = new LUDOList();
+    ludoList.renderTasks();
 
-    const ludoList=new LUDOList();
-
-    addTask.addEventListener("click", ()=>{
+    document.getElementById("add").addEventListener("click", () => {
+        const input = document.getElementById("input");
         console.log("Nuovo task aggiunto", input.value);
         ludoList.aggiungiTask(input.value);
-        input.value=""; 
-    })
-    rstTask.addEventListener("click", ()=>{
+        input.value = ""; 
+    });
+
+    document.getElementById("rst").addEventListener("click", () => {
         ludoList.resetAllTasks();
-        console.log("reset task");
-    })
-    input.addEventListener("keypress", (e)=>{          //e è l'evento che si verifica premendo invio dopo aver riempito l'input box
-        if(e.key==="Enter"){
-            console.log("Nuovo task aggiunto", input.value);
-            ludoList.aggiungiTask(input.value);
-            input.value="";       //ripulire il campo input box
-        }
-    })
+    });
 });
